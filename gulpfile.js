c4975fe19202470a6e08cf56fe4f3bbb6d0d6d58
MinimalistYing <kingying94@gmail.com>
2018-11-23 23:00:25 +0800

    publish minify version

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
 const gulp = require('gulp')
 const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
 const less = require('gulp-less')
+const LessAutoprefix = require('less-plugin-autoprefix')
+const autoprefix = new LessAutoprefix({ browsers: ['ie >= 10', 'last 5 versions'] })
+const LessPluginCleanCSS = require('less-plugin-clean-css')
+const cleancss = new LessPluginCleanCSS({ advanced: true })
 
-gulp.task('compilejs', () => gulp.src('./index.js').pipe(babel()).pipe(gulp.dest('dist')))
+gulp.task('compilejs', () => gulp.src('./index.js').pipe(babel()).pipe(uglify()).pipe(gulp.dest('dist')))
 
-gulp.task('compileless', () => gulp.src('./style.less').pipe(less()).pipe(gulp.dest('dist')))
+gulp.task('compileless', () => (
+	gulp.src('./style.less')
+		.pipe(less({plugins: [autoprefix, cleancss]}))
+		.pipe(gulp.dest('dist')
+)))
 
 gulp.task('default', ['compilejs', 'compileless'])
